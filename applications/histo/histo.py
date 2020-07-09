773e518fb0f5c517fbd2a43b50b140a4f8b5e466
Jake Krafczyk <jakekrafczyk@gmail.com>
2020-07-08 23:07:15 -0400

    Wednesday HW

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,62 @@
-# Your code here
+'''
+Print a histogram showing the word count for each word, one hash mark
+for every occurrence of the word.
 
+Output will be first ordered by the number of words, then by the word
+(alphabetically).
+
+The hash marks should be left justified two spaces after the longest
+word.
+print(f'{word}  {#*count})
+
+Case should be ignored, and all output forced to lowercase.
+
+Split the strings into words on any whitespace.
+
+Ignore each of the following characters:
+
+```
+" : ; , . - + = / \ | [ ] { } ( ) * ^ &
+```
+
+If the input contains no ignored characters, print nothing.
+
+## Hints
+
+Items: `.items()` method on a dictionary might be useful.
+
+Sorting: it's possible for `.sort()` to sort on multiple keys at once.
+
+Sorting: negatives might help where `reverse` won't.
+
+Printing: you can print a variable field width in an f-string with
+nested braces, like so `{x:{y}}`
+'''
+
+# REGEX, SPLIT AND LOWER
+# first need to import the text and split it into a list of words
+text = open('applications/histo/robin.txt','r')
+read = text.read().split()
+
+word_dict = {}
+
+for s in read:
+    s = s.lower()
+    s = ''.join(filter(str.isalpha, s))
+    # put words in a dictionary
+    if s in word_dict:
+        word_dict[s] += 1
+    else:
+        word_dict[s] = 1
+
+
+# now we need to sort and print it
+dict_items = word_dict.items()
+#print(dict_items)
+dict_items = sorted(dict_items)
+#print(dict_items)
+dict_items = sorted(dict_items,key = lambda x: x[1],reverse=True)
+
+# now print with hash marks
+for x in dict_items:
+    print(f'{x[0]}  {x[1]}')
--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,20 +1,54 @@
-# Your code here
+'''
+For expensive operations, caching the results in a lookup table speeds
+future queries.
 
+The lookup table can be built in advance by iterating over all values in
+the _domain_ of the function and recording the results.
 
+Or, more lazily, can be build as the individual values are passed in.
+'''
+import math
+import random
+
+# if x = 2 and y = 3
 def slowfun_too_slow(x, y):
+    # math.pow returns the base to the exponenet eg. pow(27,1/3) = 3
+    # 2^3 = 8
+    
     v = math.pow(x, y)
+    # a factorial is the product of an integer and all the integers below it;
+    # e.g. factorial four ( 4! ) is equal to 24. 4x3 + 4x2 + 4x1 = 24
     v = math.factorial(v)
+
+    # // is floor division, ie 9/2=4
+    #40320 // 5 = 8064
     v //= (x + y)
+    # % takes the remainder
     v %= 982451653
 
     return v
 
+cache = {}
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    # need to use caching
+    key = f'{x}{y}'
+    if key not in cache:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
+        cache[key] = v
+
+        return cache[key]
+
+    else:
+        return cache[key]
+    
 
 
 
@@ -24,3 +58,12 @@ for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
     print(f'{i}: {x},{y}: {slowfun(x, y)}')
+
+# import math
+# math.pow(2,3)
+# math.factorial(8)
+
+# 40320 //5
+
+# key = f'{2}{3}'
+# print(key)
